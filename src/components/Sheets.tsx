18c6dad41c3490c65953c944d7ab10e7ab530415
diana <diana@prodeology.com>
2024-04-11 15:26:00 +0300

    feat: partly dnd

--- src/components/Sheets.tsx
+++ src/components/Sheets.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {useSheets} from '../providers/sheets';
 import {ISheets, ThemeSettings} from "../definitions";
 import {useTheme} from "../providers/theme";
@@ -50,16 +50,43 @@ const SheetsDisplay = () => {
     }, []);
 
 
-    const headers = ['A', 'B', 'C', 'D', 'E']
+    const [headers, setHeaders] = useState<string[]>(['A', 'B', 'C', 'D', 'E'])
+    const [draggedItem, setDraggedItem] = useState<string | null>(null);
+    const handleDragStart = (index: number) => {
+        setDraggedItem(headers[index]);
+    };
+
+    const handleDragOver = (index: number) => {
+        if (draggedItem === null)
+            return;
+        const newHeaders = [...headers];
+        const draggedItemIndex = newHeaders.indexOf(draggedItem);
+        newHeaders.splice(draggedItemIndex, 1);
+        newHeaders.splice(index, 0, draggedItem);
+        setHeaders(newHeaders);
+    };
+
+    const handleDragEnd = () => {
+        setDraggedItem(null);
+    };
+
     return (
         <div className="column">
             {Object.entries(filteredSheets).length ?
                 Object.entries(filteredSheets).map(([sheetName, table]) => (
                     <div className="table" key={sheetName}>
-                        <div className="row"
-                             style={{gridTemplateColumns: `repeat(${maxCells + 1}, minmax(50px, 1fr))`}}>
+                        <div
+                            className="row"
+                            style={{gridTemplateColumns: `repeat(${maxCells + 1}, minmax(50px, 1fr))`}}>
                             {headers.slice(0, table[0].length).map((header, index) => (
-                                <div key={header} className="cell">{header}</div>
+                                <div
+                                    draggable
+                                    onDragStart={() => handleDragStart(index)}
+                                    onDragOver={() => handleDragOver(index)}
+                                    onDragEnd={handleDragEnd}
+                                    key={header}
+                                    className="cell"
+                                >{header}</div>
                             ))}
                             <div className="cell">SUM</div>
                         </div>
