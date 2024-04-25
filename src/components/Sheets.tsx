62f335f95e794380a4df1d601d1d70dfdedbb7c4
diana <diana@prodeology.com>
2024-04-25 18:28:37 +0300

    minor fixes

--- src/components/Sheets.tsx
+++ src/components/Sheets.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useMemo, useState} from 'react';
+import React, {useCallback, useMemo} from 'react';
 import {useSheets} from '../providers/sheets';
 import {ISheets, ThemeSettings} from "../definitions";
 import {useTheme} from "../providers/theme";
@@ -7,10 +7,9 @@ const SheetsDisplay = () => {
     const {sheets, selectedSheets} = useSheets();
     const {themeSettings} = useTheme();
 
-
-    const applyStyles = useCallback((rowIndex: number, cellIndex: number) => {
+    const applyStyles = useCallback((rowIndex: number, cellValue: number) => {
         const isRowEven = rowIndex % 2 === 0;
-        const isCellEven = cellIndex % 2 === 0;
+        const isCellEven = cellValue % 2 === 0;
 
         let rowStyle: Partial<ThemeSettings> = isRowEven ? themeSettings.evenRows : themeSettings.oddRows;
         let cellStyle: Partial<ThemeSettings> = isCellEven ? themeSettings.evenCellValues : themeSettings.oddCellValues;
@@ -29,78 +28,57 @@ const SheetsDisplay = () => {
         };
     }, [themeSettings]);
 
+    const calculateSum = (row: number[]) => {
+        return row.reduce((sum, value) => sum + value, 0);
+    }
 
-    const filteredSheets = useMemo(() => {
-        const filteredData: ISheets = {};
+    const view: {
+        sheets: ISheets
+        sum: { [key: string]: number[] }
+    } = useMemo(() => {
+        const filteredSheets: ISheets = {};
+        const sum: { [key: string]: number[] } = {}
         for (let key in sheets) {
-            if (selectedSheets.includes(key))
-                filteredData[key] = sheets[key];
+            if (selectedSheets.includes(key)) {
+                filteredSheets[key] = sheets[key];
+                sum[key] = sheets[key].map(row => calculateSum(row))
+            }
         }
-        return filteredData;
+        return {
+            sheets: filteredSheets,
+            sum
+        };
     }, [selectedSheets, sheets]);
 
+    /**
+     * for smooth responsive behaviour
+     */
     const maxCells = useMemo(() => {
         return Object.values(sheets).reduce((max, table) => {
             return Math.max(max, ...table.map(row => row.length));
         }, 0);
     }, [sheets]);
 
-    const calculateSum = useCallback((row: number[]) => {
-        return row.reduce((sum, value) => sum + value, 0);
-    }, []);
-
-
-    const [headers, setHeaders] = useState<string[]>(['A', 'B', 'C', 'D', 'E'])
-    const [draggedItem, setDraggedItem] = useState<string | null>(null);
-    const handleDragStart = (index: number) => {
-        setDraggedItem(headers[index]);
-    };
-
-    const handleDragOver = (index: number) => {
-        if (draggedItem === null)
-            return;
-        const newHeaders = [...headers];
-        const draggedItemIndex = newHeaders.indexOf(draggedItem);
-        newHeaders.splice(draggedItemIndex, 1);
-        newHeaders.splice(index, 0, draggedItem);
-        setHeaders(newHeaders);
-    };
-
-    const handleDragEnd = () => {
-        setDraggedItem(null);
-    };
 
     return (
         <div className="column">
-            {Object.entries(filteredSheets).length ?
-                Object.entries(filteredSheets).map(([sheetName, table]) => (
-                    <div className="table" key={sheetName}>
-                        <div
-                            className="row"
-                            style={{gridTemplateColumns: `repeat(${maxCells + 1}, minmax(50px, 1fr))`}}>
-                            {headers.slice(0, table[0].length).map((header, index) => (
-                                <div
-                                    draggable
-                                    onDragStart={() => handleDragStart(index)}
-                                    onDragOver={() => handleDragOver(index)}
-                                    onDragEnd={handleDragEnd}
-                                    key={header}
-                                    className="cell"
-                                >{header}</div>
-                            ))}
-                            <div className="cell">SUM</div>
-                        </div>
+            {Object.entries(view?.sheets).length ?
+                Object.entries(view?.sheets).map(([sheetName, table]) => (
+                    <div className="table" key={sheetName}
+                    >
                         {table.map((row, rowIndex) => (
-                            <div key={rowIndex} className="row"
-                                 style={{gridTemplateColumns: `repeat(${maxCells + 1}, minmax(50px, 1fr))`}}>
+                            <div
+                                key={rowIndex} className="row"
+                                style={{gridTemplateColumns: `repeat(${maxCells + 1}, minmax(50px, 1fr))`}}
+                            >
                                 {row.map((cell, cellIndex) => (
                                     <div
                                         key={cellIndex}
-                                        style={applyStyles(rowIndex, cellIndex)}
+                                        style={applyStyles(rowIndex + 1, cell)}
                                         className="cell"
                                     >{cell}</div>
                                 ))}
-                                <div className="cell sum">{calculateSum(row)}</div>
+                                <div className="cell sum">{view?.sum?.[sheetName]?.[rowIndex]}</div>
                             </div>
                         ))}
                     </div>
